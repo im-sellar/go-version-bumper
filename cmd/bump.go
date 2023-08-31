f045d158199d2aa09ff060455becb3d3c2bcca06
Aurélien Morice <aurelien.morice@gmail.com>
2023-08-31 15:49:50 -0400

    adding bump command

--- cmd/bump.go
+++ cmd/bump.go
@@ -1,30 +1,145 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
 )
+
+type PackageJSON map[string]interface{}
 
 // bumpCmd represents the bump command
 var bumpCmd = &cobra.Command{
 	Use:   "bump",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Upgrade the version of the package.json and package-lock.json",
+	Long: `Upgrade the version of the package.json and package-lock.json and commit the changes to git.
+The version is upgraded depending on the name of the branch. 
+If the branch name begin with "feature" then the minor number will be upgraded.
+If the branch name begin with "fix" then the patch number will be upgraded.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bump called")
+		err := updateVersionInFile("./package.json")
+		if err != nil {
+			fmt.Printf("Error updating package.json: %v\n", err)
+			return
+		}
+
+		err = updateVersionInFile("./package-lock.json")
+		if err != nil {
+			fmt.Printf("Error updating package-lock.json: %v\n", err)
+			return
+		}
+
+		fmt.Println("Version updated successfully!")
 	},
 }
 
+func updateVersionInFile(filePath string) error {
+	// Read the file
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("error reading file: %v", err)
+	}
+
+	// Get the current version
+	re := regexp.MustCompile(`"version": "([^"]+)"`)
+	matches := re.FindStringSubmatch(string(fileContent))
+	if len(matches) < 2 {
+		return fmt.Errorf("error getting current version")
+	}
+	currentVersion := matches[1]
+
+	branchName, err := getCurrentBranch()
+	if err != nil {
+		return fmt.Errorf("error getting current git branch: %v", err)
+	}
+
+	// Update the version based on the branch name
+	var newVersion string
+	if strings.Contains(branchName, "feature") {
+		// Upgrade the minor number
+		newVersion = upgradeVersion(currentVersion, 1)
+	} else if strings.Contains(branchName, "fix") {
+		// Upgrade the patch number
+		newVersion = upgradeVersion(currentVersion, 2)
+	} else {
+		return fmt.Errorf("branch name %s is not supported", branchName)
+	}
+
+	// Update the file content with the new version
+	replaced := false
+	updatedFileContent := re.ReplaceAllStringFunc(string(fileContent), func(match string) string {
+		if replaced {
+			return match
+		}
+		replaced = true
+		return `"version": "` + newVersion + `"`
+	})
+
+	// Write the updated content to the file
+	err = os.WriteFile(filePath, []byte(updatedFileContent), 0644)
+	if err != nil {
+		return fmt.Errorf("error writing to file: %v", err)
+	}
+
+	return nil
+}
+
+func upgradeVersion(version string, index int) string {
+	// Split the version string into major, minor, and patch components
+	components := strings.Split(version, ".")
+
+	// Ensure that the version string has at least three components
+	if len(components) < 3 {
+		return version
+	}
+
+	// Parse the major, minor, and patch components
+	major, err := strconv.Atoi(components[0])
+	if err != nil {
+		return version
+	}
+	minor, err := strconv.Atoi(components[1])
+	if err != nil {
+		return version
+	}
+	patch, err := strconv.Atoi(components[2])
+	if err != nil {
+		return version
+	}
+
+	// Upgrade the version based on the specified index
+	switch index {
+	case 1: // Upgrade minor number
+		minor++
+		patch = 0
+	case 2: // Upgrade patch number
+		patch++
+	default:
+		return version
+	}
+
+	// Construct the upgraded version string
+	upgradedVersion := fmt.Sprintf("%d.%d.%d", major, minor, patch)
+	return upgradedVersion
+}
+
+// getCurrentBranch returns the name of the current Git branch
+func getCurrentBranch() (string, error) {
+	cmd := exec.Command("git", "branch", "--show-current")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
 func init() {
 	rootCmd.AddCommand(bumpCmd)
 
--- go.mod
+++ go.mod
@@ -2,8 +2,10 @@ module github.com/im-sellar/go-version-bumper
 
 go 1.20
 
+
 require (
+	github.com/spf13/cobra v1.7.0
+
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
